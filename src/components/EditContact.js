2100ca2d4bcc6ae5258df394f0fa3aa2555b6f88
mihirzalavadiya <mihirzalavadiya@gmail.com>
2021-10-22 22:24:35 +0530

    added dispatch

--- src/components/EditContact.js
+++ src/components/EditContact.js
@@ -1,32 +1,94 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { Link, useParams } from 'react-router-dom'
+import { toast } from 'react-toastify';
 
 const EditContact = () => {
 
-    const {id} = useParams();
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [number, setNumber] = useState("");
+
+    const { id } = useParams();
+    const contacts = useSelector(state => state);
+    const dispatch = useDispatch(); 
+    const history = useHistory();
+    const currentContact = contacts.find(contact => contact.id === parseInt(id));
+
+    useEffect(() => {
+        if (currentContact) {
+            setName(currentContact.name);
+            setEmail(currentContact.email);
+            setNumber(currentContact.number);
+        }
+    }, [currentContact]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const checkEmail = contacts.find(contact => contact.id !== parseInt(id) && contact.email === email);
+        const checkNumber = contacts.find(contact => contact.id !== parseInt(id) && contact.number === parseInt(number));
+
+        if (!name || !email || !number) {
+            return toast.warning("Please fill in all fields!")
+        }
+
+        if (number.length < 10 || number.length > 10) {
+            return toast.warning("Please enter valid number!")
+        }
+
+        if (checkEmail) {
+            return toast.error("This email is already Exists!")
+        }
+
+        if (checkNumber) {
+            return toast.error("This number is already Exists!")
+        }
+
+        const data = {
+            id: parseInt(id),
+            name,
+            email,
+            number
+        };
+
+        dispatch({ type: "UPDATE_CONTACT", payload: data })
+        toast.success("User a   dded Successfully!");
+        history.push("/");
+    };
 
     return (
         <div className="container">
-            <h1 className="display-3 my-4 text-center">Edit User {id}</h1>
-            <div className="row">
-                <div className="col-md-6 shadow p-5 mx-auto">
-                    <form>
-                        <div className="form-group p-1">
-                            <input type="text" placeholder="Name" className="form-control" />
+            {
+                currentContact ? (
+                    <>
+
+                        <h1 className="display-3 my-4 text-center">Edit User</h1>
+                        <div className="row">
+                            <div className="col-md-6 shadow p-5 mx-auto">
+                                <form onSubmit={handleSubmit}>
+                                    <div className="form-group p-1">
+                                        <input type="text" placeholder="Name" className="form-control" value={name} onChange={e => setName(e.target.value)} />
+                                    </div>
+                                    <div className="form-group p-1">
+                                        <input type="emial" placeholder="Email" className="form-control" value={email} onChange={e => setEmail(e.target.value)} />
+                                    </div>
+                                    <div className="form-group p-1">
+                                        <input type="number" placeholder="Contact Number" className="form-control" value={number} onChange={e => setNumber(e.target.value)} />
+                                    </div>
+                                    <div className="form-group p-1">
+                                        <input type="submit" value="Update User" className="btn btn-dark" />
+                                        <Link to="/" className="btn btn-danger m-2">Cancel</Link>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
-                        <div className="form-group p-1">
-                            <input type="emial" placeholder="Email" className="form-control" />
-                        </div>
-                        <div className="form-group p-1">
-                            <input type="number" placeholder="Contact Number" className="form-control" />
-                        </div>
-                        <div className="form-group p-1">
-                            <input type="submit" value="Update User" className="btn btn-dark" />
-                            <Link to="/" className="btn btn-danger m-2">Cancel</Link>
-                        </div>
-                    </form>
-                </div>
-            </div>
+                    </>
+                ) :
+                    (<h3 className="display-5 font-weight-bold my-5 text-center">User contact {id} is not existes.</h3>)
+            }
         </div>
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,48 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 const Home = () => {
+
+    const contacts = useSelector(state => state);
+
     return (
         <div className="container">
             <div className="row">
-                <div className="col-md-12 my-5 text-right">
-                    <Link to="/add"  className="btn btn-outline-dark">Add</Link>
+                <div className="col-md-12 my-5 alignr">
+                    <Link to="/add" className="btn btn-outline-danger">Add User</Link>
                 </div>
-                <div className="col-md-6 mx-auto">
-                    <h1>Contact App</h1>
+                <div className="col-md-10 mx-auto text-center">
+                    <table className="table table-hover">
+                        <thead className="text-white bg-dark text-center">
+                            <tr>
+                                <th scope="col">No.</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Number</th>
+                                <th scope="col"></th>
+                                <th scope="col"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                contacts.map((contact, id) => (
+                                    <tr key={id}>
+                                        <td>{id + 1}</td>
+                                        <td>{contact.name}</td>
+                                        <td>{contact.email}</td>
+                                        <td>{contact.number}</td>
+                                        <td>
+                                            <Link to={`/edit/${contact.id}`} className="btn btn-small btn-success mx-2">Edit</Link>
+                                        </td>
+                                        <td>
+                                            <button type="button" className="btn btn-small btn-danger">Delete</button>
+                                        </td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
